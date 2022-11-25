1d977b63a112045b39556a5993428235683283b3
elisabeth <96701818+Eli-sa-1983@users.noreply.github.com>
2022-11-25 20:40:25 +0100

    Update Gre_project.py

--- Gre_project.py
+++ Gre_project.py
@@ -5,6 +5,21 @@ from PIL import Image
 st.title('Les Dott de Grenoble!')
 st.sidebar.header('Velos, Trotinettes')
 
+def main_page():
+    st.markdown("# Main page 🎈")
+    st.sidebar.markdown("# Main page 🎈")
 
+def page2():
+    st.markdown("# Page 2 ❄️")
+    st.sidebar.markdown("# Page 2 ❄️")
+
+def page3():
+    st.markdown("# Page 3 🎉")
+    st.sidebar.markdown("# Page 3 🎉")
+
+page_names_to_funcs = {
+    "Main Page": main_page,
+    "Page 2": page2,
+    "Page 3": page3,
 
 st.image('gretrott.jpeg')
